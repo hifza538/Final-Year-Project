5f51ce0e60db2425443afd3ef6bdd83db856e07b
Hifza <hifzaahsan135@gmail.com>
2026-07-02 21:25:17 +0500

    fix: resolve next() Type-error in user pre save hook

--- backend/models/User.js
+++ backend/models/User.js
@@ -96,11 +96,10 @@ const userSchema = new mongoose.Schema(
 );
 
 // hash the password before saving the user document
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // method to compare entered password with hashed password in the database
